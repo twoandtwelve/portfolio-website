27d7c9d14f6259627d2cefbdea004247aae0ccfe
twoandtwelve <jackykim573@gmail.com>
2023-07-27 03:54:17 +1200

    setting up files

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/jackykim/Library/Mobile Documents/com~apple~CloudDocs/PERSONAL WORK related stuff/git/portfolio-web/app/page": [
+      "/Users/jackykim/Library/Mobile Documents/com~apple~CloudDocs/PERSONAL WORK related stuff/git/portfolio-web/styles/globals.css"
+    ]
+  },
+  "/Users/jackykim/Library/Mobile Documents/com~apple~CloudDocs/PERSONAL WORK related stuff/git/portfolio-web/app/layout.tsx": [
+    "/Users/jackykim/Library/Mobile Documents/com~apple~CloudDocs/PERSONAL WORK related stuff/git/portfolio-web/styles/globals.css"
+  ]
+}
---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fjackykim%2FLibrary%2FMobile%20Documents%2Fcom~apple~CloudDocs%2FPERSONAL%20WORK%20related%20stuff%2Fgit%2Fportfolio-web%2Fstyles%2Fglobals.css&server=false!":
+/*!*************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fjackykim%2FLibrary%2FMobile%20Documents%2Fcom~apple~CloudDocs%2FPERSONAL%20WORK%20related%20stuff%2Fgit%2Fportfolio-web%2Fstyles%2Fglobals.css&server=false! ***!
+  \*************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_styles_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./styles/globals.css */ \"(app-client)/./styles/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGamFja3lraW0lMkZMaWJyYXJ5JTJGTW9iaWxlJTIwRG9jdW1lbnRzJTJGY29tfmFwcGxlfkNsb3VkRG9jcyUyRlBFUlNPTkFMJTIwV09SSyUyMHJlbGF0ZWQlMjBzdHVmZiUyRmdpdCUyRnBvcnRmb2xpby13ZWIlMkZzdHlsZXMlMkZnbG9iYWxzLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLE9BQU8sNExBQWlLIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ZjAzZCJdLCJzb3VyY2VzQ29udGVudCI6WyIoKCkgPT4gaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImxhenlcIiAqLyBcIi9Vc2Vycy9qYWNreWtpbS9MaWJyYXJ5L01vYmlsZSBEb2N1bWVudHMvY29tfmFwcGxlfkNsb3VkRG9jcy9QRVJTT05BTCBXT1JLIHJlbGF0ZWQgc3R1ZmYvZ2l0L3BvcnRmb2xpby13ZWIvc3R5bGVzL2dsb2JhbHMuY3NzXCIpKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fjackykim%2FLibrary%2FMobile%20Documents%2Fcom~apple~CloudDocs%2FPERSONAL%20WORK%20related%20stuff%2Fgit%2Fportfolio-web%2Fstyles%2Fglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fjackykim%2FLibrary%2FMobile%20Documents%2Fcom~apple~CloudDocs%2FPERSONAL%20WORK%20related%20stuff%2Fgit%2Fportfolio-web%2Fstyles%2Fglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
