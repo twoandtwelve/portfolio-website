83de8fabef2a3464df780a2eb206dc9ba6e2fc39
twoandtwelve <jackykim573@gmail.com>
2023-07-29 19:55:27 +1200

    added projects

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -6,25 +6,20 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
   {
-    name: "project 1",
-    description: "description here",
-    image: "/imagehere.jpg",
-    github: "github link",
-    link: "link",
+    name: "Graph Calculator (assignment)",
+    description:
+      "A command-line application built using Java. The user is able to input their own graphs into the app and determine various properties of them. First-breadth search and first-depth search are also implemented.",
+    image: "/graph_calculator.png",
+    github: "https://github.com/twoandtwelve/graph-calculator.git",
+    link: "https://github.com/twoandtwelve/graph-calculator.git",
   },
   {
-    name: "project 1",
-    description: "description here",
-    image: "/imagehere.jpg",
-    github: "github link",
-    link: "link",
-  },
-  {
-    name: "project 1",
-    description: "description here",
-    image: "/imagehere.jpg",
-    github: "github link",
-    link: "link",
+    name: "Morra (assignment)",
+    description:
+      "A command-line game built using Java. The user is able to play the hand game Morra against an AI with four selectable difficulties.",
+    image: "/morra.png",
+    github: "https://github.com/twoandtwelve/morra.git",
+    link: "https://github.com/twoandtwelve/morra.git",
   },
 ];
 
@@ -52,7 +47,7 @@ const ProjectsSection = () => {
                       />
                     </Link>
                   </div>
-                  <div className="mt-12 md:w-1/2">
+                  <div className="mt-12 md:w-3/5">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
                       {project.description}
